46dccbadac6b2da60854043ae9274f8584a47668
Charlene Zhang <zhan5449@umn.edu>
2020-01-27 02:36:43 +0800

    Second commit (finished assignment)

--- R/week2.R
+++ R/week2.R
@@ -1,3 +1,12 @@
 # Import and Labeling
 rt_df <- read.csv(file="/Users/zhan5449/Google Drive/Data Science/Week 2/Week-2-Project/Data/week2.csv",header=TRUE)
 rt_df$condition <- factor(rt_df$condition,levels=c("A","B"),labels=c("Control","Experimental"))
+rt_df$gender <- factor(rt_df$gender,levels=c("M","F","T"),labels=c("Male","Female","Transgender"))
+
+# Analysis
+mean(rt_df$rt)
+rt_f_df <- rt_df[rt_df$gender=="Female",]
+str(rt_f_df)
+hist(rt_f_df$rt)
+datasets <- list(rt_df,rt_f_df)
+datasets[[1]]["rt"]
